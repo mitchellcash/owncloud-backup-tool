1e67a1240d16c36ad9847c30a1faa270ad5a85ec
Mitchell Cash <mitchell@mitchellcash.com>
2016-12-15 21:21:56 +1000

    Working script and config to backup the ownCloud database

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ownCloud Backup Tool
 ====================
+
+A script to backup your ownCloud database as well as config/ and data/ directories to Amazon s3
+
+Usage
+-----
--- owncloud-backup-tool.sh
+++ owncloud-backup-tool.sh
@@ -1,1 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# A script to backup your ownCloud database as well as config/ and data/ directories to Amazon s3
+
+# Load config file TODO: make this more secure
+source .owncloud-backup-tool/owncloud-backup-tool.conf
+
+# MySQL/MariaDB Database dump
+mysqldump --lock-tables -h "$host" -u "$user" -p "$password" "$db_name" > owncloud-dbbackup_`date +"%Y%m%d"`.bak
