682d4eb811aa987257d802d0b9a374ece7da0ae2
Mitchell Cash <mitchell@mitchellcash.com>
2016-12-17 17:06:37 +1000

    Add support for backing up data directories and sync to s3

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ ownCloud Backup Tool
 
 A script to backup your ownCloud database as well as config/ and data/ directories to Amazon s3
 
+Requirements
+------------
+
+* [aws-cli](https://github.com/aws/aws-cli)
+
 Usage
 -----
--- owncloud-backup-tool.sh
+++ owncloud-backup-tool.sh
@@ -5,5 +5,14 @@
 # Load config file TODO: make this more secure
 source .owncloud-backup-tool/owncloud-backup-tool.conf
 
-# MySQL/MariaDB Database dump
-mysqldump --lock-tables -h "$host" -u "$user" --password="$password" "$db_name" > owncloud-dbbackup_`date +"%Y%m%d"`.bak
+# Make directory to store local backups
+mkdir -p ~/owncloud-backup/database
+
+# MySQL/MariaDB database dump
+mysqldump --lock-tables -h "$host" -u "$user" --password="$password" "$db_name" > ~/owncloud-backup/database/owncloud-dbbackup_`date +"%Y%m%d"`.bak
+
+# Locally backup the config/ and data/ directories
+rsync -Aax /var/www/owncloud/config /var/www/owncloud/data ~/owncloud-backup
+
+# Backup the database dump, config/ and data/ directories to Amazon S3
+aws s3 sync ~/owncloud-backup s3://$s3_bucket_name
