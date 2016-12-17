60b6c1b730b25b05081e471e8d503ea8b4c2d65c
Mitchell Cash <mitchell@mitchellcash.com>
2016-12-17 17:17:12 +1000

    Small code cleanup

--- owncloud-backup-tool.sh
+++ owncloud-backup-tool.sh
@@ -9,10 +9,10 @@ source .owncloud-backup-tool/owncloud-backup-tool.conf
 mkdir -p ~/owncloud-backup/database
 
 # MySQL/MariaDB database dump
-mysqldump --lock-tables -h "$host" -u "$user" --password="$password" "$db_name" > ~/owncloud-backup/database/owncloud-dbbackup_`date +"%Y%m%d"`.bak
+mysqldump --lock-tables -h "$host" -u "$user" --password="$password" "$db_name" > ~/owncloud-backup/database/owncloud-dbbackup_"$(date +"%Y%m%d")".bak
 
 # Locally backup the config/ and data/ directories
 rsync -Aax /var/www/owncloud/config /var/www/owncloud/data ~/owncloud-backup
 
 # Backup the database dump, config/ and data/ directories to Amazon S3
-aws s3 sync ~/owncloud-backup s3://$s3_bucket_name
+aws s3 sync ~/owncloud-backup s3://"$s3_bucket_name"
